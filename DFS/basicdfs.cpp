a4e7f7bbd122caaeb14e79b71ec9b4dabc1d8df0
shoreful islam <abcd@gamil.com>
2025-08-17 15:15:20 +0600

    this is my practice dsa code

--- DFS/basicdfs.cpp
+++ DFS/basicdfs.cpp
@@ -1,38 +1,61 @@
-#include <bits/stdc++.h>
+//In the name of ALLAH
+//bismillahir rahmanir rahim
+
+#include<bits/stdc++.h>
 using namespace std;
+#define int long long int
 
-const int mx=30;
-vector<int>adj[mx];
-bool visited[mx];
+#define pb push_back
+#define endl '\n'
+#define mod 1000000007
+ 
+const double eps = 1e-9;
+const int inf = 2000000000;
+const int infl = 9000000000000000000;
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-void DFS(int source)
+int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+
+signed main()
 {
-    visited[source]=1;
-    for(auto node:adj[source])
-    {
-        if(!visited[node])
-        {
-             DFS(node);
-        }
-    }
-}
+    optimize();
 
-int main()
-{
-    int n,m;
-    cin>>n>>m;
-    int u,v;
-    for(int i=0;i<m;i++)
+    int TC;
+    cin>>TC;
+    while (TC--)
     {
-       cin>>u>>v;
-       adj[u].push_back(v);
-       adj[v].push_back(u);
-    }
-    DFS(1);
+       int n;
+       cin>>n;
+       vector<int>a(n+2),b(n+2),c(n+2),d(n+2),ans(n+2);
+       for(int i=1;i<=n;i++)
+       {
+         cin>>a[i];
+       }
+       for(int i=1;i<=n;i++)
+       {
+         cin>>b[i];
+         c[i]=c[i-1]+b[i];
+       }
 
-    for(int i=1;i<=n;i++)
-    {
-        cout<<visited[i]<<" ";
-    }
-    cout<<endl;
+       for(int i=1;i<=n;i++)
+       {
+          int  pos=upper_bound(c.begin()+i,c.begin()+n+1,a[i]+c[i-1])-c.begin();
+          d[i]++;
+          d[pos]--;
+          ans[pos]+= a[i]-(c[pos-1]-c[i-1]);
+          d[i]+=d[i-1];
+          ans[i]+=(d[i]*b[i]);
+       }
+
+       for(int i=1;i<=n;i++)
+       {
+          cout<<ans[i]<<" ";
+       }
+       cout<<endl;
+    }  
 }
